0ffbdc1454d46c8ebc47983b16cf4f9e3a509452
davoudkeramati <davoud.keramati@gmail.com>
2025-04-30 23:53:31 +0330

    Added Edit files

--- DailyIncomeExpenseProject/die_app/models.py
+++ DailyIncomeExpenseProject/die_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
